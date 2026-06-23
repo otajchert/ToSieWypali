6d643c00ca6fa5ede9c04ffd99983c100bc07489
otajchert <otajchert@gmail.com>
2026-06-24 01:26:13 +0200

    add: add domain

--- backend/src/main/java/com/tsw/config/SecurityConfig.java
+++ backend/src/main/java/com/tsw/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://tosiewypali.pl", "https://www.tosiewypali.pl"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
